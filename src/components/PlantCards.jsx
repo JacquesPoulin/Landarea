3a8f1650a7286c5cdca3e2c03b3abe5d87926ba5
AntsoTech <antso-rakotoson@hotmail.fr>
2022-05-12 16:20:03 +0200

    finalized card shaping v1

--- src/components/PlantCards.jsx
+++ src/components/PlantCards.jsx
@@ -1,26 +1,30 @@
 import React from "react";
 
-const PlantCards = () => {
+const PlantCards = (props) => {
   return (
     <div className="flex justify-center py-5">
       <div className="w-11/12 shadow-xl relative font-main rounded-xl">
         <img
-          src="https://cdn.shopify.com/s/files/1/0109/7079/1998/products/85096-00-BAKI_c40ba6be-2326-45f6-a9f0-11d1537adc34_823x.jpg?v=1652229436"
+          src={props.url}
           alt="Brugmansia suaveolens Tricolor"
           className="object-cover w-full h-50 rounded-xl"
         />
         {/* Informations plante */}
         <div className="flex flex-row absolute bottom-0 bg-slate-50/80 w-full px-2 py-2">
           <div className="flex flex-col">
-            <h1 className="z-3 text-xl">Brugmansia suaveolens Tricolor</h1>
-            <h2 className="z-3 text-xs">Plante d'extérieur</h2>
-            <h2 className="z-3 text-xs">A retirer à Jardiland, Anglet</h2>
-            <h2 className="z-3 text-xs font-primary">avant le 22/04/2022</h2>
+            <h1 className="z-3 text-xl">{props.name}</h1>
+            <h2 className="z-3 text-xs">{props.plantCare}</h2>
+            <h2 className="z-3 text-xs">
+              A retirer à {props.place}, {props.city}
+            </h2>
+            <h2 className="z-3 text-xs font-primary">
+              avant le {props.withdrawalLimit}
+            </h2>
           </div>
 
           {/* Prix plante et bouton réserver */}
           <div className="flex flex-col justify-around">
-            <h2 className="text-right text-2xl">3€</h2>
+            <h2 className="text-right text-2xl">{props.price} €</h2>
             <button className="rounded-md bg-emerald-800 px-4 text-white">
               {" "}
               Réserver{" "}
--- src/page/Homepage/Home.jsx
+++ src/page/Homepage/Home.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import PlantCards from "../../components/PlantCards.jsx";
+import plants from "../../../data/plants.js";
 
 const Home = () => {
   return (
     <div>
-      <h1></h1>
-      <PlantCards />
+      <h1>Hero page</h1>
+
+      <div>
+        {" "}
+        {plants.map((plant, index) => (
+          <PlantCards key={index} {...plant} />
+        ))}
+      </div>
     </div>
   );
 };
