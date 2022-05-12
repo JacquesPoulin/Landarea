7bd832865d6f9cae782286fe567a575e176be30b
AntsoTech <antso-rakotoson@hotmail.fr>
2022-05-12 18:37:35 +0200

    footer V1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Footer = () => {
+  const [email, setEmail] = useState("");
+
   return (
     <div>
-      <h1>FOOTER</h1>
+      <div className="flex flex-col justify-center items-center bg-greeny py-3 font-regular">
+        <label> Contactez-nous</label>
+        <input
+          type="text"
+          value={email}
+          placeholder="mon email"
+          className="border-2 my-2 text-center"
+        />
+      </div>
+      <div className="flex flex-col justify-center items-center bg-greeny">
+        <h2 className="font-regular"> Ils nous soutiennent</h2>
+      </div>
     </div>
   );
 };
