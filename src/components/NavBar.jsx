1e115020cddf81edb6615a761ab038368da44760
JacquesPoulin <jacques.poulin64@gmail.com>
2022-05-12 11:28:18 +0200

    PROJECT INIT

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+const NavBar = () => {
+  return (
+    <nav className="fixed z-10 w-full h-10">
+      <ul className="flex flex-row justify-center p-5 m-0 text-xl list-none cursor-pointer bg-slate-500 text-slate-200">
+        <li className=" hover:text-slate-800">NAVBAR</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
