29b13680f6af621e61ca0049005f0d907ed87fbe
Mr BDR <mirebordier@gmail.com>
2022-05-12 16:51:20 +0200

    hero2

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import "./App.css";
 function App() {
   return (
     <div>
-      {/* <NavBar /> */}
+      <NavBar />
       <Home {...heroes} />
       <Add {...heroes} />
       <Store {...heroes} />
