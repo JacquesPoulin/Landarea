f3e2cbf6af97611a2c5ee1226ea117f7c668af2c
JohnnyHiriart <johnnyhc@hotmail.com>
2022-05-12 16:04:32 +0200

    filter select ok

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 
-const Filter = ({location,setlocation}) => {
+const Filter = () => {
   return (
-    <div>
-      <label>Découvrir à </label>
+    <div className="font-main">
+      <label >
+      Découvrir à {" "}
+          <select name="filter" id="filter" class="border-4 border-solid ">
+            <option value="">choisisez votre ville</option>
+            <option value="1">Anglet</option>
+            <option value="2">Bayonne</option>
+            <option value="3">Biarritz</option>
+            <option value="4">Bidart</option>
+            <option value="5">Boucau</option>
+            <option value="6">Hendaye</option>
+            <option value="7">Ondres</option>
+            <option value="8">Tarnos</option>
+            <option value="9">Urcuit</option>
+            <option value="10">Urt</option>
+            </select>
+        </label>
+     
     </div>
   );
 };
--- src/page/Homepage/Home.jsx
+++ src/page/Homepage/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import PlantCards from "../../components/PlantCards.jsx";
+import Filter from "../../components/Filter.jsx";
 
 const Home = () => {
   return (
     <div>
       <h1>Bob</h1>
+      <Filter/>
       <PlantCards />
     </div>
   );
