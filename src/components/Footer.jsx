8f72a78f818f21d205b612e2549ec851d3954a96
AntsoTech <antso-rakotoson@hotmail.fr>
2022-05-12 23:23:38 +0200

    logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,8 @@ const Footer = () => {
 
   return (
     <div>
-      <div className="flex flex-col justify-center items-center bg-greeny py-3 font-regular">
-        <label> Contactez-nous</label>
+      <div className="flex flex-col justify-center items-center bg-greeny py-3 font-regular w-full">
+        <label className="text-white"> Contactez-nous</label>
         <input
           type="text"
           value={email}
@@ -16,7 +16,7 @@ const Footer = () => {
         <div className="w-2/3 h-[1px] p-0 bg-creamy m-0"></div>
       </div>
       <div className="flex flex-col justify-center items-center bg-greeny/80 py-7">
-        <h2 className="font-regular mb-4"> Ils nous soutiennent</h2>
+        <h2 className="font-regular mb-4 text-white"> Ils nous soutiennent</h2>
         <div className="flex flex-wrap justify-around gap-12 m-5">
           {/* <img src="../assets/images/nasa.svg" alt="nasa" width="50" /> */}
           <img src="../assets/images/eelv.png" alt="eelv" width="170" />
@@ -31,6 +31,22 @@ const Footer = () => {
           />
 
           <img src="../assets/images/wcs.png" alt="wcs" width="160" />
+        </div>
+        <div className="flex flex-col flex-wrap justify-center items-center bg-greeny py-3 font-regular w-full">
+          <h2 className="text-white py-2">Retrouvez-nous sur les réseaux </h2>
+          <div className="flex flex-row justify-around w-1/3">
+            <img
+              src="../assets/images/instagram.svg"
+              alt="instagram"
+              width={30}
+            />
+            <img src="../assets/images/twitter.svg" alt="twitter" width={30} />
+            <img
+              src="../assets/images/facebook.svg"
+              alt="facebook"
+              width={30}
+            />
+          </div>
         </div>
       </div>
     </div>
