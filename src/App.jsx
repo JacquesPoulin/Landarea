0fdeb3aab10c7d7a02162da7381e5079f7cb6603
JacquesPoulin <jacques.poulin64@gmail.com>
2022-05-12 12:15:00 +0200

    [INIT] : V2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import "./App.css";
-import NavBar from "./components/NavBar";
-
 
 function App() {
   return (
     <div>
-      <NavBar/>
+      <h1>READY FOR THE HACKATHON</h1>
     </div>
   );
 }
