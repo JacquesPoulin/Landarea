0d47e2a551ab1f27f913299d36e1b41dcc03eaa5
JacquesPoulin <jacques.poulin64@gmail.com>
2022-05-13 04:22:22 +0200

    [UPDATES]

--- data/popups.js
+++ data/popups.js
@@ -1,21 +1,21 @@
 const popups = [
   {
     id: 1,
-    title: "Petits bobos",
+    title: "Pour_les_petits_bobos",
     url: "../assets/images/store2.jpeg",
     description:
       "La vie n'est pas un long fleuve tranquille alors on s’occupe de vos plantes et on vous donne des conseils d’entretien",
   },
   {
     id: 2,
-    title: "Petits orphelins",
+    title: "Pour_les_petits_orphelins",
     url: "../assets/images/store1.jpeg",
     description:
       "Déposez vos plantes, racontez-nous votre histoire et participez à nos ventes privées tous les samedis",
   },
   {
     id: 3,
-    title: "Seconde beauté",
+    title: "Pour_une_seconde beauté",
     url: "../assets/images/heroes6.jpg",
     description:
       "Parfois nos fleurs ne trouvent pas l'amour, alors on leur redonne une seconde beauté. Découvrez notre sélection de fleurs ! ",
