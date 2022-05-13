ed66f9f2fa55bb5116409f3c49fa3f174f478868
JohnnyHiriart <johnnyhc@hotmail.com>
2022-05-13 06:35:51 +0200

    filtre les city

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -2,32 +2,40 @@ import React from "react";
 import plants from "../../data/plants";
 
 const Filter = ({ setCityChoice, setIsFilter }) => {
+  const [data, setData] = React.useState(plants);
+  React.useEffect(() => {
+    const newArray = [];
+    data.map(
+      (plant) =>
+        newArray.findIndex((p) => p.city == plant.city) === -1 &&
+        newArray.push(plant)
+    );
+    setData(newArray);
+  }, []);
   return (
     <div className="m-6 text-center font-main">
       <div className="flex flex-col justify-center">
         <label
           htmlFor="city"
-          className="font-medium flex flex-col justify-center"
+          className="flex flex-col justify-center font-medium"
         >
           Choisisez votre ville{" "}
           <select
-            className="rounded font-main text-center m-auto mt-4 mb-4 border-b-4 border-green-800"
+            className="m-auto mt-4 mb-4 text-center border-b-4 border-green-800 rounded font-main"
             onChange={(e) => setCityChoice(e.target.value)}
           >
-            <option
-              value=""
-              className="text-black
-            text-center rounded-md"
-            >
+            <option value="" className="text-center text-black rounded-md">
               Toutes
             </option>
-            {plants
-              .sort((a, b) => (a.city > b.city ? 1 : -1))
-              .map((plant) => (
-                <option key={plant.id} value={plant.city}>
-                  {plant.city}
-                </option>
-              ))}
+            {data &&
+              data
+                // .filter((plant)=>(plant.city))
+                .sort((a, b) => (a.city > b.city ? 1 : -1))
+                .map((plant) => (
+                  <option key={plant.id} value={plant.city}>
+                    {plant.city}
+                  </option>
+                ))}
           </select>
         </label>
       </div>
