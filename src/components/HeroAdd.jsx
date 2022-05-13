0c25ca150243cd85d6dd2879ed551118025912b1
Mr BDR <mirebordier@gmail.com>
2022-05-13 07:29:21 +0200

    [DARK]:dark is done, its 7:30am broooooooo

--- src/components/HeroAdd.jsx
+++ src/components/HeroAdd.jsx
@@ -7,10 +7,10 @@ const HeroAdd = ({isDark}) => {
     <div
       className={`flex flex-col m-0 bg-[url('../assets/images/heroes2.jpg')] h-96 bg-cover`}
     >
-      <h1 className={isDark?`font-primary m-5 mt-20 mb-10 text-4xl text-center text-slate-50`:`font-primary m-5 mt-20 mb-10 text-4xl text-center text-red-600`}>
+      <h1 className={!isDark?`font-primary m-5 mt-20 mb-10 text-4xl text-center text-slate-50`:`font-primary m-5 mt-20 mb-10 text-4xl text-center text-red-600`}>
         {heroes[1].title}
       </h1>
-      <p className={isDark?`font-regular m-5 text-xl text-center text-slate-50`:`font-regular m-5 text-xl text-center text-yellow-500`}>
+      <p className={!isDark?`font-regular m-5 text-xl text-center text-slate-50`:`font-regular m-5 text-xl text-center text-yellow-500`}>
         {heroes[1].txt}
       </p>
     </div>
--- src/components/HeroHome.jsx
+++ src/components/HeroHome.jsx
@@ -7,10 +7,10 @@ const HeroHome = ({isDark}) => {
     <div
       className={`flex flex-col justify-center m-0 bg-[url('../assets/images/heroes51.png')] h-96 bg-cover  `}
     >
-      <h1 className={isDark?`m-5 mt-20 mb-10 text-4xl text-center font-primary text-slate-50`:`m-5 mt-20 mb-10 text-4xl text-center font-primary text-red-600`}>
+      <h1 className={!isDark?`m-5 mt-20 mb-10 text-4xl text-center font-primary text-slate-50`:`m-5 mt-20 mb-10 text-4xl text-center font-primary text-red-600`}>
         {heroes[0].title}
       </h1>
-      <p className={isDark?`m-5 text-xl text-center font-regular text-slate-50`:`m-5 text-xl text-center font-regular text-yellow-500`}>
+      <p className={!isDark?`m-5 text-xl text-center font-regular text-slate-50`:`m-5 text-xl text-center font-regular text-yellow-500`}>
         {heroes[0].txt}
       </p>
     </div>
